19ca522907a49e2eb3a0557c82ff5ed376539e04
LeandroSQ <leandrosoaresquevedo@gmail.com>
2024-06-20 04:59:29 -0300

    fix: Using gifski over ffmpeg
    Former-commit-id: 2188cd4ee22d4d72d06940ebf8de925cf73657de [formerly 01252d9f6cefce9ae2e4297df3d723a1cd28bc67]
    Former-commit-id: dc1bf4e3896164d5b088502eae4894becfa2aa90

--- tools/gif-packer.sh
+++ tools/gif-packer.sh
@@ -15,7 +15,12 @@ if [ ! -d "$OUTPUT_DIR" ]; then
 fi
 
 # Generate pallete
-ffmpeg -y -framerate $FPS -i "$INPUT_DIR/%d.png" -vf "fps=$FPS,palettegen=stats_mode=full" $OUTPUT_DIR/palette.png
+# ffmpeg -y -framerate $FPS -i "$INPUT_DIR/%d.png" -vf "fps=$FPS,palettegen=stats_mode=full" $OUTPUT_DIR/palette.png
 
 # Generate gif
-ffmpeg -y -framerate $FPS -i "$INPUT_DIR/%d.png" -i $OUTPUT_DIR/palette.png -lavfi "fps=$FPS [x]; [x][1:v] paletteuse=dither=sierra2_4a:diff_mode=rectangle" -f gif $OUTPUT_DIR/output.gif
+# ffmpeg -y -framerate $FPS -i "$INPUT_DIR/%d.png" \
+# 	-vf "fps=$FPS,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse=dither=bayer:bayer_scale=5" \
+# 	-c:v gifski -pix_fmt rgb24 \
+# 	$OUTPUT_DIR/output.gif
+
+gifski -o $OUTPUT_DIR/output.gif $INPUT_DIR/*.png
